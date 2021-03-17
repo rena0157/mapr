a97e3b747b6c51fe4c2f2a6b2ff57cb113f19569
Adam Renaud <adam.renaud@abicbuilds.com>
2021-03-17 16:07:35 -0400

    Project Setup

---
+++ src/Mapr/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Mapr
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/Mapr.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Mapr.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
