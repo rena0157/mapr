93d62013db2a66d3b08f5559abf816795af08755
Adam Renaud <adam.renaud@abicbuilds.com>
2021-03-17 19:52:52 -0400

    Setting update to build number

--- build/Build.cs
+++ build/Build.cs
@@ -25,7 +25,7 @@ class Build : NukeBuild
     
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion(Framework = "netcoreapp3.1")] readonly GitVersion GitVersion;
+    [GitVersion(Framework = "netcoreapp3.1", UpdateBuildNumber = true)] readonly GitVersion GitVersion;
     
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
