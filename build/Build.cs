39a12109916585a3ae2b0a438e545123a48caf73
Adam Renaud <adam.renaud@abicbuilds.com>
2021-12-06 20:45:54 -0500

    Updating GitVersion Tool

--- build/Build.cs
+++ build/Build.cs
@@ -26,7 +26,7 @@ class Build : NukeBuild
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion(Framework = "netcoreapp3.1", UpdateBuildNumber = true)] readonly GitVersion GitVersion;
+    [GitVersion(Framework = "net5.0", UpdateBuildNumber = true)] readonly GitVersion GitVersion;
     
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
